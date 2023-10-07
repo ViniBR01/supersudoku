10323f11a4f4fd0b54ecbd41bc07faf49743c632
Vini Goncalves <vinicius@skylarkwireless.com>
2023-10-07 12:57:08 -0500

    Creates basic unit tests for the class Game

--- test/Game_test.cpp
+++ test/Game_test.cpp
@@ -4,5 +4,33 @@
 TEST(Game, DefaultConstructor) {
   Game game;
   // Assert that the game is initialized with correct size and all zeros
-  EXPECT_EQ(0, 0);
+  EXPECT_EQ(game.remove_entry(0), 0);
+  EXPECT_FALSE(game.fully_filled());
+  EXPECT_FALSE(game.correctly_solved());
+}
+
+TEST(Game, fill_board) {
+  Game game;
+  game.fill_board();
+  EXPECT_FALSE(game.fully_filled());
+  EXPECT_EQ(game.remove_entry(1), 2);
+  //EXPECT_TRUE(game.has_unique_solution());
+}
+
+TEST(Game, solve_backtracking) {
+  Game game;
+  game.fill_board();
+  game.solve_backtracking();
+  EXPECT_TRUE(game.fully_filled());
+  EXPECT_TRUE(game.correctly_solved());
+}
+
+TEST(Game, has_unique_solution) {
+  Game game;
+  EXPECT_FALSE(game.has_unique_solution());
+  game.fill_board();
+  EXPECT_TRUE(game.has_unique_solution());
+  game.clear_board();
+  game.generate_new_game();
+  EXPECT_TRUE(game.has_unique_solution());
 }
