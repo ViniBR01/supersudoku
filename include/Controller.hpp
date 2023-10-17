ba35b55f3b250a031ec470aa59510d335d780e4e
Vini Goncalves <vinicius@skylarkwireless.com>
2023-10-16 19:42:14 -0500

    Defines base signatures for Controller class

---
+++ include/Controller.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class Controller {
+public:
+  void start_game();
+private:
+  void main_menu();
+  void play_game();
+  void input_field();
+  void check_correctness();
+};
---
+++ src/Controller.cpp
@@ -0,0 +1,17 @@
+#include "Controller.hpp"
+
+void Controller::start_game() {
+  this->main_menu();
+}
+
+void Controller::main_menu() {
+}
+
+void Controller::play_game() {
+}
+
+void Controller::input_field() {
+}
+
+void Controller::check_correctness() {
+}
