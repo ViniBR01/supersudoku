d595e2caffe9dad034f7fa581c4c8ca387775b26
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-05 09:18:42 -0600

    Adds support for pencil candidates in the cell representation

--- version02/src/cell.hpp
+++ version02/src/cell.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
-template <typename elementType>
+template <typename elementType, size_t puzzleCardinality>
 class Cell
 {
 public:
   bool isClue();
   bool setElement(elementType element);
   elementType getElement(elementType element);
+  bool isPencilCandidate(elementType element);
+  bool addPencilCandidate(elementType element);
+  bool removePencilCandidate(elementType element);
 private:
   elementType element_;
   bool isClue_{};
+  std::set<elementType> pencilCandidates_;
 };
 
 bool Cell::isClue()
@@ -32,3 +36,27 @@ elementType Cell::getElement()
   return element_;
 }
   
+bool isPencilCandidate(elementType element)
+{
+  return pencilCandidates_.contains(element);
+}
+
+bool addPencilCandidate(elementType element)
+{
+  if (pencilCandidates_.contains(element))
+    {
+      return false;
+    }
+  pencilCandidates_.insert(element);
+  return true;
+}
+
+bool removePencilCandidate(elementType element)
+{
+  if (not pencilCandidates_.contains(element))
+    {
+      return false;
+    }
+  pencilCandidates_.erase(element);
+  return true;
+}
