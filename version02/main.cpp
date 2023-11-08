c637c154959a179849151cb6ed8ca41b356017d0
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-08 13:11:36 -0600

    Fully define the template class inside of header file

--- version02/main.cpp
+++ version02/main.cpp
@@ -5,8 +5,11 @@
 int main() {
   std::cout << "Hello, world." << std::endl;
 
-  Cell<int, 9> cell;
+  sudoku::Cell<int, 9> cell;
   std::cout << cell.isClue() << std::endl;
+  std::cout << cell.getElement() << std::endl;
+  cell.setElement(5);
+  std::cout << cell.getElement() << std::endl;
   
   return 0;
 }
--- version02/src/cell.hpp
+++ version02/src/cell.hpp
@@ -2,18 +2,71 @@
 
 #include <set>
 
-template <typename elementType, size_t puzzleCardinality>
-class Cell
-{
-public:
-  bool isClue();
-  bool setElement(elementType element);
-  elementType getElement();
-  bool isPencilCandidate(elementType element);
-  bool addPencilCandidate(elementType element);
-  bool removePencilCandidate(elementType element);
-private:
-  elementType element_{};
-  bool isClue_{};
-  std::set<elementType> pencilCandidates_;
-};
+namespace sudoku {
+  template <typename elementType, size_t puzzleCardinality>
+  class Cell
+  {
+  public:
+    bool isClue();
+    bool setElement(elementType element);
+    elementType getElement();
+    bool isPencilCandidate(elementType element);
+    bool addPencilCandidate(elementType element);
+    bool removePencilCandidate(elementType element);
+  private:
+    elementType element_{};
+    bool isClue_{};
+    std::set<elementType> pencilCandidates_;
+  };
+
+  template <typename elementType, size_t puzzleCardinality>
+  bool Cell<elementType, puzzleCardinality>::isClue()
+  {
+    return isClue_;
+  }
+  
+  template <typename elementType, size_t puzzleCardinality>
+  bool Cell<elementType, puzzleCardinality>::setElement(elementType element)
+  {
+    if (isClue_)
+      {
+	return false;
+      }
+    element_ = element;
+    return true;
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  elementType Cell<elementType, puzzleCardinality>::getElement()
+  {
+    return element_;
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  bool Cell<elementType, puzzleCardinality>::isPencilCandidate(elementType element)
+  {
+    return pencilCandidates_.contains(element);
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  bool Cell<elementType, puzzleCardinality>::addPencilCandidate(elementType element)
+  {
+    if (pencilCandidates_.contains(element))
+      {
+	return false;
+      }
+    pencilCandidates_.insert(element);
+    return true;
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  bool Cell<elementType, puzzleCardinality>::removePencilCandidate(elementType element)
+  {
+    if (not pencilCandidates_.contains(element))
+      {
+	return false;
+      }
+    pencilCandidates_.erase(element);
+    return true;
+  }
+}
