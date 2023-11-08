12ffb9edea662a7af4dd1ab76b638ec2a2229f3c
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-07 21:21:02 -0600

    Splits cell class into header and implementation files

--- version02/src/cell.hpp
+++ version02/src/cell.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <set>
+
 template <typename elementType, size_t puzzleCardinality>
 class Cell
 {
 public:
   bool isClue();
   bool setElement(elementType element);
-  elementType getElement(elementType element);
+  elementType getElement();
   bool isPencilCandidate(elementType element);
   bool addPencilCandidate(elementType element);
   bool removePencilCandidate(elementType element);
@@ -15,48 +17,3 @@ private:
   bool isClue_{};
   std::set<elementType> pencilCandidates_;
 };
-
-bool Cell::isClue()
-{
-  return isClue_;
-}
-
-bool Cell::setElement(elementType element)
-{
-  if (isClue_)
-    {
-      return false;
-    }
-  element_ = element;
-  return true;
-}
-
-elementType Cell::getElement()
-{
-  return element_;
-}
-  
-bool isPencilCandidate(elementType element)
-{
-  return pencilCandidates_.contains(element);
-}
-
-bool addPencilCandidate(elementType element)
-{
-  if (pencilCandidates_.contains(element))
-    {
-      return false;
-    }
-  pencilCandidates_.insert(element);
-  return true;
-}
-
-bool removePencilCandidate(elementType element)
-{
-  if (not pencilCandidates_.contains(element))
-    {
-      return false;
-    }
-  pencilCandidates_.erase(element);
-  return true;
-}
