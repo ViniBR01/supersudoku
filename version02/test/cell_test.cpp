bcd0545570842e1fb1f05f8bd88718193c79e22b
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-10 04:12:56 -0600

    Covers all methods of cell with unit tests

--- version02/test/cell_test.cpp
+++ version02/test/cell_test.cpp
@@ -14,3 +14,37 @@ TEST(CellTest, ClueConstructor) {
   EXPECT_EQ(cell.isClue(), true);
   EXPECT_EQ(cell.getElement(), 3);
 }
+
+TEST(CellTest, ElementManipulation) {
+  int clueValue = 3;
+  sudoku::Cell<int,9> clueCell(clueValue);
+  sudoku::Cell<int,9> openCell;
+  int testValue = 5;
+  
+  EXPECT_FALSE(clueCell.setElement(0));
+  EXPECT_FALSE(clueCell.setElement(testValue));
+  EXPECT_EQ(clueCell.getElement(), clueValue);
+  
+  EXPECT_TRUE(openCell.setElement(testValue));
+  EXPECT_EQ(openCell.getElement(), testValue);
+  EXPECT_TRUE(openCell.setElement(0));
+  EXPECT_EQ(openCell.getElement(), 0);
+}
+
+TEST(CellTest, PencilCandidates) {
+  int clueValue = 3;
+  sudoku::Cell<int,9> clueCell(clueValue);
+  sudoku::Cell<int,9> openCell;
+  int testValue = 5;
+
+  EXPECT_FALSE(clueCell.isPencilCandidate(testValue));
+  EXPECT_FALSE(clueCell.addPencilCandidate(testValue));
+  EXPECT_FALSE(clueCell.removePencilCandidate(testValue));
+
+  EXPECT_FALSE(openCell.isPencilCandidate(testValue));
+  EXPECT_FALSE(openCell.removePencilCandidate(testValue));
+  EXPECT_TRUE(openCell.addPencilCandidate(testValue));
+  EXPECT_TRUE(openCell.isPencilCandidate(testValue));
+  EXPECT_TRUE(openCell.removePencilCandidate(testValue));
+  EXPECT_FALSE(openCell.isPencilCandidate(testValue));
+}
