23c35788597db0db55c1d6d06bc617c847344c8e
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-10 03:52:10 -0600

    Adds basic tests for the 2 contructors

--- version02/test/cell_test.cpp
+++ version02/test/cell_test.cpp
@@ -2,8 +2,15 @@
 
 #include "cell.hpp"
 
-sudoku::Cell<int,9> cell;
-
-TEST(CellTest, BasicAssertions) {
+TEST(CellTest, DefaultConstructor) {
+  sudoku::Cell<int,9> cell;
   EXPECT_EQ(cell.isClue(), false);
+  EXPECT_EQ(cell.getElement(), 0);
+}
+
+TEST(CellTest, ClueConstructor) {
+  int clueValue = 3;
+  sudoku::Cell<int,9> cell(clueValue);
+  EXPECT_EQ(cell.isClue(), true);
+  EXPECT_EQ(cell.getElement(), 3);
 }
