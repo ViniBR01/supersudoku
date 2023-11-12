bc757a652f9de5e107a58adca4447bfbcc723a48
Vini Goncalves <vinicius@skylarkwireless.com>
2023-11-11 18:01:16 -0600

    Implements accessors to cells

--- version02/src/grid.hpp
+++ version02/src/grid.hpp
@@ -11,9 +11,13 @@ namespace sudoku {
   public:
     Grid<elementType, puzzleCardinality>();
     size_t getSize();
+    Cell<elementType, puzzleCardinality>& getCell(size_t index);
+    Cell<elementType, puzzleCardinality>& getCell(size_t row, size_t column);
   private:
     static constexpr size_t puzzleSize_ = puzzleCardinality * puzzleCardinality;
     std::array<Cell<elementType, puzzleCardinality>, puzzleSize_> cells_;
+
+    size_t getIndex_(size_t row, size_t column);
   };
 
   template <typename elementType, size_t puzzleCardinality>
@@ -21,8 +25,30 @@ namespace sudoku {
   {}
 
   template <typename elementType, size_t puzzleCardinality>
-  size_t Grid<elementType, puzzleCardinality>::getSize()
+  size_t
+  Grid<elementType, puzzleCardinality>::getSize()
   {
     return puzzleSize_;
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  Cell<elementType, puzzleCardinality>&
+  Grid<elementType, puzzleCardinality>::getCell(size_t index)
+  {
+    return cells_[index];
+  }
+  
+  template <typename elementType, size_t puzzleCardinality>
+  Cell<elementType, puzzleCardinality>&
+  Grid<elementType, puzzleCardinality>::getCell(size_t row, size_t column)
+  {
+    return cells_[getIndex_(row, column)];
+  }
+
+  template <typename elementType, size_t puzzleCardinality>
+  size_t
+  Grid<elementType, puzzleCardinality>::getIndex_(size_t row, size_t column)
+  {
+    return row+column*puzzleCardinality;
   }
 }
